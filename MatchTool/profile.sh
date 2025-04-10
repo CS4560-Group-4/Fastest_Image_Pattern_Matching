5806a6e337dea92fbe4f91c07b0687e0c409537c
Wiliam Dokov <wdokov@tudelft.nl>
2025-04-10 12:47:15 +0200

    Added timer class and perf script

---
+++ MatchTool/profile.sh
@@ -0,0 +1,5 @@
+perf record -F 99 -a -g -- ./out/main tests/10/Src10.bmp tests/10/Dst10.jpg
+perf script > out.perf 
+~/FlameGraph/stackcollapse-perf.pl out.perf > out.folded
+rm perf.data
+rm out.perf
